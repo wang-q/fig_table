502e33cbbba7f1a410dc02a7ee3efa232207b6cd
wang-q <wang-q@outlook.com>
2015-11-01 23:53:19 +0800

    New style

--- xlsx2xls.pl
+++ xlsx2xls.pl
@@ -3,8 +3,8 @@ use strict;
 use warnings;
 use autodie;
 
-use Getopt::Long;
-use Pod::Usage;
+use Getopt::Long qw(HelpMessage);
+use FindBin;
 use YAML qw(Dump Load DumpFile LoadFile);
 
 use Win32::OLE qw(in);
@@ -12,40 +12,44 @@ use Win32::OLE::Const;
 use Win32::OLE::Variant;
 use Win32::OLE::NLS qw(:LOCALE :DATE);
 use Win32::OLE::Const 'Microsoft Excel';
+$Win32::OLE::Warn = 3;    # die on errors...
 
 use File::Find::Rule;
-use File::Spec;
-
-$Win32::OLE::Warn = 3;    # die on errors...
+use Path::Tiny;
 
 #----------------------------------------------------------#
 # GetOpt section
 #----------------------------------------------------------#
-# running options
-my $dir = '.';
 
-my $suffix = "*.xlsx";
+=head1 NAME
 
-my $csv;
+xlsx2xls.pl - convert xlsx to xls or csv
 
-my $man  = 0;
-my $help = 0;
+=head1 SYNOPSIS
+
+    perl xlsx2xls.pl [options]
+      Options:
+        --help          -?          brief help message
+        --dir           -d  STR     dir containing xlsx files
+        --suffix        -s  STR     suffix, default is [*.xlsx]
+        --sheet             STR     one sheet
+        --csv                       convert to csv
+
+    perl xlsx2xls.pl -d stat -s "*.chart.xlsx"
+
+=cut
 
 GetOptions(
-    'help|?'     => \$help,
-    'man'        => \$man,
-    'd|dir=s'    => \$dir,
-    's|suffix=s' => \$suffix,
-    'csv'        => \$csv,
-) or pod2usage(2);
-
-pod2usage(1) if $help;
-pod2usage( -exitstatus => 0, -verbose => 2 ) if $man;
+    'help|?' => sub { HelpMessage(0) },
+    'dir|d=s'    => \( my $dir    = '.' ),
+    'suffix|s=s' => \( my $suffix = "*.xlsx" ),
+    'csv'        => \my $csv,
+    'sheet=s'    => \my $sheetnames,
+) or HelpMessage(1);
 
 #----------------------------------------------------------#
 # init
 #----------------------------------------------------------#
-
 my $excel;    # excel object
 unless ( $excel = Win32::OLE->new("Excel.Application") ) {
     die "Cannot init Excel.Application\n";
@@ -56,7 +60,7 @@ my @files = File::Find::Rule->file->name($suffix)->in($dir);
 printf "\n----Total $suffix Files: %4s----\n\n", scalar @files;
 
 for my $file (@files) {
-    $file = File::Spec->rel2abs($file);
+    $file = path($file)->absolute->stringify;
     print $file, "\n";
     my $newbook = $excel->Workbooks->Open($file)
         or die "Cannot open xlsx file\n";
@@ -66,11 +70,11 @@ for my $file (@files) {
     # xlExcel9795 43 Excel9795 # this doesn't work
     # xlCSV       6  CSV
     if ($csv) {
-        $outfile =~ s/\.\w+$//i;
+        $outfile =~ s/\.\w+$/.csv/i;
         $newbook->SaveAs( $outfile, 6 );
     }
     else {
-        $outfile =~ s/\.\w+$/xls/i;
+        $outfile =~ s/\.\w+$/.xls/i;
         $newbook->SaveAs( $outfile, 56 );
     }
 }
@@ -78,21 +82,3 @@ for my $file (@files) {
 $excel->Quit;
 
 __END__
-
-=head1 NAME
-
-xlsx2xls.pl - convert xlsx to xls or csv
-
-=head1 SYNOPSIS
-
-    perl xlsx2xls.pl [options]
-      Options:
-        --help              brief help message
-        --man               full documentation
-        -d, --dir STR       dir containing xlsx files
-        -s, --suffix STR    suffix, default is *.xlsx
-        --csv               convert to csv
-
-    perl xlsx2xls.pl -d stat -s *.chart.xlsx
-
-=cut
