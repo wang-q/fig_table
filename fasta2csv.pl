d48199bc581d12bca837d2ef1ffcd196fde1fb2b
wang-q <wang-q@outlook.com>
2016-12-15 01:06:29 +0800

    No Pod::Usage

--- fasta2csv.pl
+++ fasta2csv.pl
@@ -1,40 +1,45 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use autodie;
 
 use Getopt::Long;
-use Pod::Usage;
-use YAML qw(Dump Load DumpFile LoadFile);
+use YAML::Syck;
 
-use List::Util qw(first max maxstr min minstr reduce shuffle sum);
-use List::MoreUtils qw(any all);
+use List::Util;
+use List::MoreUtils::PP;
 
 #----------------------------------------------------------#
 # GetOpt Section
 #----------------------------------------------------------#
-my $infile        = "../116_112.fasta";
-my $wrap          = 60;
-my $spacing       = 0;
-my $outfile       = "";
-my $has_reference = 1;
-my $filter        = "ingroup";            # outgroup or ingroup
 
-my $man  = 0;
-my $help = 0;
+=head1 NAME
+
+fasta2csv.pl - Extract polymorphic sites from fasta alignment file
+
+=head1 SYNOPSIS
+
+    fasta2csv.pl [options]
+      Options:
+        --help            brief help message
+        --infile          input file name (full path)
+        --wrap            wrap number
+        --spacing         wrapped line spacing
+        --outfile         output file name
+        --outgroup        has outgroup?
+        --filter          ingroup or outgroup sites
+
+=cut
 
 GetOptions(
-    'help|?'      => \$help,
-    'man'         => \$man,
-    'infile=s'    => \$infile,
-    'wrap=i'      => \$wrap,
-    'spacing=i'   => \$spacing,
-    'outfile=s'   => \$outfile,
-    'reference=s' => \$has_reference,
-    'filter=s'    => \$filter,
-) or pod2usage(2);
-
-pod2usage(1) if $help;
-pod2usage( -exitstatus => 0, -verbose => 2 ) if $man;
+    'help|?'   => sub { Getopt::Long::HelpMessage(0) },
+    'infile=s' => \my $infile,
+    'wrap=i'     => \( my $wrap         = 60 ),
+    'spacing=i'  => \( my $spacing      = 0 ),
+    'outfile=s'  => \( my $outfile      = "" ),
+    'outgroup=s' => \( my $has_outgroup = 1 ),
+    'filter=s'   => \( my $filter       = "ingroup" ),
+) or Getopt::Long::HelpMessage(1);
 
 unless ($outfile) {
     $outfile = $infile;
@@ -70,14 +75,14 @@ foreach my $current_line (@file_contents) {
         my $current_seq_name = $seq_names[-1];
         $seqs{$current_seq_name} .= $current_line;
     }
-    else { # Blank line, do nothing
+    else {    # Blank line, do nothing
     }
 }
 
 # seq names
 my $ref_name;
 my @total_names;
-if ($has_reference) {
+if ($has_outgroup) {
     @total_names = @seq_names;
     $ref_name    = shift @seq_names;
 }
@@ -86,7 +91,7 @@ else {
 }
 
 # seq number
-my $seq_number   = scalar @seq_names;
+#my $seq_number   = scalar @seq_names;
 my $total_number = scalar @total_names;
 
 # seq length
@@ -94,7 +99,7 @@ my @total_lengths;
 foreach (@total_names) {
     push @total_lengths, length $seqs{$_};
 }
-unless ( all { $_ == $total_lengths[0] } @total_lengths ) {
+unless ( List::MoreUtils::PP::all { $_ == $total_lengths[0] } @total_lengths ) {
     die "Seq length error.\n";
 }
 else {
@@ -129,12 +134,11 @@ for ( my $i = 1; $i <= $total_lengths[0]; $i++ ) {
         @cur_in_column = @cur_column;
     }
 
-    if ( minstr(@cur_in_column) ne maxstr(@cur_in_column) ) {
+    if ( List::Util::minstr(@cur_in_column) ne List::Util::maxstr(@cur_in_column) ) {
         unshift @cur_column, $i;
         $snp_count++;
         foreach ( 0 .. $total_number ) {
-            push @{   $result_matrix->[ $_
-                    + $section * ( $total_number + 1 + $spacing ) ] },
+            push @{ $result_matrix->[ $_ + $section * ( $total_number + 1 + $spacing ) ] },
                 $cur_column[$_];
         }
 
@@ -143,8 +147,7 @@ for ( my $i = 1; $i <= $total_lengths[0]; $i++ ) {
             $section++;
             print "Section: $section\n";
             foreach ( 0 .. $total_number ) {
-                push @{   $result_matrix->[ $_
-                        + $section * ( $total_number + 1 + $spacing ) ] },
+                push @{ $result_matrix->[ $_ + $section * ( $total_number + 1 + $spacing ) ] },
                     ( " ", @total_names )[$_];
             }
         }
@@ -167,42 +170,3 @@ foreach (@$result_matrix) {
 close $csv_fh;
 
 __END__
-
-=head1 NAME
-
-    fasta2csv.pl - Extract polymorphic sites from fasta alignment file
-
-=head1 SYNOPSIS
-
-    fasta2csv.pl [options]
-     Options:
-       --help            brief help message
-       --man             full documentation
-       --infile          input file name (full path)
-       --wrap            wrap number 
-       --spacing         wrapped line spacing
-       --outfile         output file name
-       --reference       has reference?
-       --filter          ingroup or outgroup sites
-       
-
-=head1 OPTIONS
-
-=over 8
-
-=item B<-help>
-
-Print a brief help message and exits.
-
-=item B<-man>
-
-Prints the manual page and exits.
-
-=back
-
-=head1 DESCRIPTION
-
-B<This program> will read the given input file(s) and do someting
-useful with the contents thereof.
-
-=cut
